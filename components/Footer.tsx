e72f8a80c030153929dc8896bbe782148ce78b22
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2022-10-02 22:17:10 -0400

    fix: add link to arya's github

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,14 +8,27 @@
 import Link from "next/link";
 
 export default function Footer() {
-    return (
-        <footer className="flex flex-col gap-3 p-4 bg-black">
-                <p className="text-gray-400 text-sm text-center">&copy; {new Date().getFullYear()} John Fraser SS Computer Science Club. All Rights Reserved.</p>
+  return (
+    <footer className="flex flex-col gap-3 p-4 bg-black">
+      <p className="text-gray-400 text-sm text-center">&copy; {new Date().getFullYear()} John Fraser SS Computer Science Club. All Rights Reserved.</p>
 
 
-                <hr className="mx-16 md:mx-32 lg:mx-64 border-gray-600" />
+      <hr className="mx-16 md:mx-32 lg:mx-64 border-gray-600" />
 
-                <p className="text-gray-400 text-sm text-center">Made by <a href="https://www.aritrosaha.ca/" className="text-blue-500 hover:text-blue-700 duration-200">Aritro Saha</a> and Arya Holmukhe</p>
-        </footer>
-    );
+      <p className="text-gray-400 text-sm text-center">
+        Made by
+        {" "}
+        <a href="https://www.aritrosaha.ca/" className="text-blue-500 hover:text-blue-700 duration-200">
+          Aritro Saha
+        </a>
+        {" "}
+        and
+        {" "}
+        <a href="https://github.com/Aryaholmukhe" className="text-blue-500 hover:text-blue-700 duration-200">
+          Arya Holmukhe
+        </a>
+        .
+      </p>
+    </footer>
+  );
 }
